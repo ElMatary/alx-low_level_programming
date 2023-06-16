65e66fe85460efc4ea3a701b425082299f4f3ae0
ElMatary <mahmoudelmatary724@yahoo.com>
2023-06-16 11:20:48 -0700

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: program that prints the alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (char <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n')
+	return (0);
+}
