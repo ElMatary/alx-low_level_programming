d5eb5d26924a52e01660fb831058fc2b2b2a1e45
ElMatary <mahmoudelmatary724@yahoo.com>
2023-06-20 13:19:27 -0700

    function that prints 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Description: print_alphabet_x10 - using function _putchar to
+ * print 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int n, ch
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
