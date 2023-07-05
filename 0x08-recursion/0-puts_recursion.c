5f9cf50b2dd3e32e2bae23cbbd9b80c7d7cee311
ElMatary <mahmoudelmatary724@yahoo.com>
2023-07-05 12:07:23 -0700

    function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -18,6 +18,6 @@ void _puts_recursion(char *s)
 	else
 	{
 		_putchar(s[0]);
-		_put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 
-int _putchar(char c);
 void _puts_recursion(char *s);
 
 #endif
